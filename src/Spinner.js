47b01650ced56a305418ee8d118a317465eb4333
Tony Watkins <tony@llifion.net>
2020-12-11 18:42:39 +0000

    Recommended way to specify default props is to create a defaultProps object for our component - that way if the component is used multiple times, the default only has to be set in one place

--- src/Spinner.js
+++ src/Spinner.js
@@ -5,10 +5,14 @@ const Spinner = (props) => {
     return (
         <div className="ui active dimmer">
             <div className="ui big text loader">
-                {props.message || 'Loading...'}
+                {props.message}
             </div>
         </div>
     );
+};
+
+Spinner.defaultProps = {
+    message: 'Loading...'
 };
 
 export default Spinner;
