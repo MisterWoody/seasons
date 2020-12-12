64791c9271e19f0f6d4d1e46e2f2fbb26dfd6abf
Tony Watkins <tony@llifion.net>
2020-12-12 11:30:05 +0000

    Remove temp console log of props message

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Spinner = (props) => {
-    console.log(props.message);
     return (
         <div className="ui active dimmer">
             <div className="ui big text loader">
